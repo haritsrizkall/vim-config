16dab6e89281643d6bdd00e4622aff967016e981
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2023-10-08 15:31:10 +0700

    mapping nvim tree

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,5 +1,14 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
+
 require'nvim-tree'.setup({
+    sort_by = " ase_sensitike",
     view = {
-        width = 20,
+        width = 30,
     },
 })
--- lua/ranjau/remap.lua
+++ lua/ranjau/remap.lua
@@ -47,3 +47,4 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
