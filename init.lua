fde168178a0ec7cbd063ea963daffc9480243ea4
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2023-12-12 22:34:35 +0700

    add github copilot

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("ranjau")
 require("ranjau.packer")
+vim.g.copilot_assume_mapped = true
 print("Hello Rizkal")
 
--- lua/ranjau/packer.lua
+++ lua/ranjau/packer.lua
@@ -53,6 +53,8 @@ return require('packer').startup(function(use)
 			{'L3MON4D3/LuaSnip'},     -- Required
 		}
 	}
+
+  use("github/copilot.vim")
 end)
 
 
